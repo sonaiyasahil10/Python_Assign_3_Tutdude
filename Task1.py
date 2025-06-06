1a0e4a1b02178db7020ad16a219e7ce7bb8c8888
sonaiyasahil10 <sonaiyasahil10@gmail.com>
2025-06-06 23:25:21 +0530

    Updated Solutions

--- Task1.py
+++ Task1.py
@@ -2,7 +2,9 @@
 num = int(input("Enter a Number: "))
 
 def factorial(n):
-    if n<2:
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    if n < 2:
         return 1
     return n * factorial(n-1)
 
